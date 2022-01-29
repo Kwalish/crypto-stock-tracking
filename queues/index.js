068111ee7b24b2071fbd2dbdd1172eb0305ad544
Fabien Martin <fabienmartin@Fabiens-MacBook-Pro.local>
2022-01-29 19:08:32 +0900

    upgraded to latest bullboard version

--- queues/index.js
+++ queues/index.js
@@ -1,11 +1,23 @@
-const { createBullBoard } = require('bull-board');
-const { BullAdapter } = require('bull-board/bullAdapter');
+const { createBullBoard } = require('@bull-board/api');
+const { BullAdapter } = require('@bull-board/api/bullAdapter');
+const { ExpressAdapter } = require('@bull-board/express');
 const { portfolioQueue } = require('./portfolio');
 const { queryCryptoQueue } = require('./queryCrypto');
 
-const { router } = createBullBoard([
-  new BullAdapter(portfolioQueue),
-  new BullAdapter(queryCryptoQueue),
-]);
+const serverAdapter = new ExpressAdapter();
+
+const {
+  addQueue, removeQueue, setQueues, replaceQueues
+} = createBullBoard({
+  queues: [
+    new BullAdapter(portfolioQueue),
+    new BullAdapter(queryCryptoQueue),
+  ],
+  serverAdapter,
+});
+
+serverAdapter.setBasePath('/admin/queues');
+
+const router = serverAdapter.getRouter();
 
 module.exports = router;
